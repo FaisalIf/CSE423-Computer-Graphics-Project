9d1eef4185840f33b41fcaa770de503028ff7a8e
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-08-18 21:52:01 +0600

    Create entities module: Basic and compound shapes

---
+++ entities.py
@@ -0,0 +1,50 @@
+class Shape:
+    def __init__(self, x, y, z, l, b, h):
+        # Center position
+        self.x = x
+        self.y = y
+        self.z = z
+
+        # Bounding box
+        self.x_min = x - l/2
+        self.y_min = y - b/2
+        self.z_min = z - h/2
+        self.x_max = self.x_min+l
+        self.y_max = self.y_min+b
+        self.z_max = self.z_min+h
+
+    def check_collision(self, other):
+        col_x_min = self.x_min < other.x_min < self.x_max
+        col_y_min = self.y_min < other.y_min < self.y_max
+        col_z_min = self.z_min < other.z_min < self.z_max
+        col_x_max = self.x_min < other.x_max < self.x_max
+        col_y_max = self.y_min < other.y_max < self.y_max
+        col_z_max = self.z_min < other.z_max < self.z_max
+
+        col_x = col_x_min or col_x_max
+        col_y = col_y_min or col_y_max
+        col_z = col_z_min or col_z_max
+
+        if col_x and col_y and col_z: return True
+        else: return False
+
+class CompoundShape(Shape):
+    def __init__(self, *shapes):
+        self.shapes = shapes
+
+        # Center position
+        all_x = [s.x for s in shapes]
+        all_y = [s.y for s in shapes]
+        all_z = [s.z for s in shapes]
+        self.x = sum(all_x)/len(all_x)
+        self.y = sum(all_y)/len(all_y)
+        self.z = sum(all_z)/len(all_z)
+
+
+        # Bounding Box
+        self.x_min = min(s.x_min for s in shapes)
+        self.y_min = min(s.y_min for s in shapes)
+        self.z_min = min(s.z_min for s in shapes)
+        self.x_max = max(s.x_max for s in shapes)
+        self.y_max = max(s.y_max for s in shapes)
+        self.z_max = max(s.z_max for s in shapes)
