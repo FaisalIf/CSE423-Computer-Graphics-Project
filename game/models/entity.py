fb2988497372071ae5b6356de549ed951b23ed2c
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-08-25 09:17:03 +0600

    Trim the fat

--- game/models/entity.py
+++ game/models/entity.py
@@ -12,8 +12,6 @@ COLORS = {
 def get_color(name):
     if name in COLORS:
         return COLORS[name]
-    else:
-        raise KeyError(f'Color {name} not found in COLORS dictionary')
 
 # ==============================================================================
 
