2474e0a96f389f3878b6c572993fc5d3ba106958
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-08-25 19:19:21 +0600

    Improve 3rd person camera

--- main.py
+++ main.py
@@ -3,16 +3,54 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from game.models.models import *
 
-def camera():
+class Cam3rd:
+    def __init__(self, ex, ey, ez, cx, cy, cz, ux=0, uy=0, uz=1):
+        self.eye_x = ex
+        self.eye_y = ey
+        self.eye_z = ez
+        self.cen_x = cx
+        self.cen_y = cy
+        self.cen_z = cz
+        self.up_x = ux
+        self.up_y = uy
+        self.up_z = uz
+    def move(self, dx, dy, dz):
+        self.eye_x += dx
+        self.eye_y += dy
+        self.eye_z += dz
+        self.cen_x += dx
+        self.cen_y += dy
+        self.cen_z += dz
+    def get_cam(self):
+        return (self.eye_x, self.eye_y, self.eye_z,
+                self.cen_x, self.cen_y, self.cen_z,
+                self.up_x,  self.up_y,  self.up_z)
+
+#def Cam1st:
+#    def __init__(self, entity):
+#        self.entity = entity
+#    def get_cam(self):
+#        ex, ey, ez = self.entity.x, self.entity.y, self.entity.z
+#        rx, ry, rz = self.entity.rx, self.entity.ry, self.entity.rz
+#        cx = ex + math.cos(math.radians(rx))
+#        cy = ex + math.sin(math.radians(ry))
+#        cz = ex + math.cos(math.radians(rx))
+#
+#        return (self.entity.x, self.entity.y, self.entity.z,
+#                self.cen_x, self.cen_y, self.cen_z,
+#                self.up_x,  self.up_y,  self.up_z)
+        
+
+def camera(cam):
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    gluLookAt(300, -300, 300,
-              0,    0,   0,
-              0,    0,   1)
+    view = cam.get_cam()
+    gluLookAt(*view)
 
 def display():
+    global cam1
     glClear(GL_COLOR_BUFFER_BIT)
-    camera()
+    camera(cam1)
     global floor
     floor.draw()
     glutSwapBuffers()
@@ -21,7 +59,16 @@ def animate():
     glutPostRedisplay()
     
 def keys(key, x, y):
-    pass
+    global cam1
+    if key == b'w':
+        cam1.move(0, 1, 0)
+    if key == b'a':
+        cam1.move(-1, 0, 0)
+    if key == b's':
+        cam1.move(0, -1, 0)
+    if key == b'd':
+        cam1.move(1, 0, 1)        
+        
 
 def special_keys(key, x, y):
     pass
@@ -41,8 +88,10 @@ def main():
     glutCreateWindow(window_title)
 
     # Entities
-    global floor
+    global floor, cam1
     floor = Box('red', 0, 0, 5, 0, 0, 0, 200, 800, 10)
+    cam1 = Cam3rd(300, -300, 300, 0, 0, 0, 0, 0, 1)
+    
     # Event Listners
     glutDisplayFunc(display)
     glutIdleFunc(animate)
@@ -51,7 +100,7 @@ def main():
     glutMouseFunc(clicks)
 
     # Camera Setup
-    fov_y, aspect_ratio, near, far = 130, (win_w/win_h), 0.1, 5000
+    fov_y, aspect_ratio, near, far = 120, (win_w/win_h), 0.1, 5000
     glMatrixMode(GL_PROJECTION)
     gluPerspective(fov_y, aspect_ratio, near, far)
 
